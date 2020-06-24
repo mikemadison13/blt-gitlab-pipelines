34e60b50201be80978f2b66b02069b4a253a5c87
Mike Madison <mike.madison@acquia.com>
2020-06-24 11:29:09 -0700

    updating gitlab detection for ci environment detector.

--- src/Blt/Plugin/EnvironmentDetector/GitlabDetector.php
+++ src/Blt/Plugin/EnvironmentDetector/GitlabDetector.php
@@ -5,6 +5,10 @@ namespace Acquia\GitlabPipelines\Blt\Plugin\EnvironmentDetector;
 use Acquia\Blt\Robo\Common\EnvironmentDetector;
 
 class GitlabDetector extends EnvironmentDetector {
+    public static function getCiEnv() {
+        return isset($_ENV['GITLAB_CI']) ? 'gitlab' : null;
+    }
+
     public static function getCiSettingsFile() {
         return sprintf('%s/vendor/mikemadison13/blt-gitlab-pipelines/settings/gitlab.settings.php', dirname(DRUPAL_ROOT));
     }
