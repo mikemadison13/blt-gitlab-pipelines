5b0bc19a45bcb1a730a5dcc58f303e5405f2c324
Michael Sherron <msherron@gmail.com>
2020-12-16 12:11:50 -0700

    Issue #2: Fixes for gitlab deploy logic and SSH key management.

--- scripts/gitlab/deploy_artifact.sh
+++ scripts/gitlab/deploy_artifact.sh
@@ -2,6 +2,6 @@
 
 set -ev
 
-vendor/bin/blt artifact:deploy --commit-msg "Automated commit by Gitlab Pipelines for Build $BUILD_BUILDID" --branch "$BUILD_SOURCEBRANCH-build" --no-interaction --verbose
+vendor/bin/blt artifact:deploy --commit-msg "Automated commit by Gitlab Pipelines for Build $CI_PIPELINE_IID" --branch "$CI_COMMIT_BRANCH-build" --no-interaction --verbose
 
 set +v
