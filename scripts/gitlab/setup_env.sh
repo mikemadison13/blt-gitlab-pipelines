25d91097b1acf24ab7b5133322ae8c04e20b679a
Mike Madison <mike.madison@acquia.com>
2021-09-24 10:02:45 -0700

    POC for updated gitlab config.

--- scripts/gitlab/setup_env.sh
+++ scripts/gitlab/setup_env.sh
@@ -13,9 +13,21 @@ touch /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
 echo 'memory_limit = -1' >> /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
 
 # Create a MySQL database for drupal to use
-echo "CREATE DATABASE IF NOT EXISTS \`drupal8\`;" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
-echo "CREATE USER 'drupal8'@'%' IDENTIFIED BY 'drupal8';" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
-echo "GRANT ALL ON drupal8.* TO 'drupal8'@'%';" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
+echo "CREATE DATABASE IF NOT EXISTS \`drupal\`;" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
+echo "CREATE USER 'drupal'@'%' IDENTIFIED BY 'drupal';" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
+echo "GRANT ALL ON drupal.* TO 'drupal'@'%';" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
 echo "FLUSH PRIVILEGES;" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
+
+# Install Node JS and NPM
+apt-get update
+apt-get install libxss1
+curl -sL https://deb.nodesource.com/setup_${NODE_VERSION} | bash -
+apt install -y nodejs
+npm install -g npm
+
+# Install Google Chrome and Cleanup Binary
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
 
 set +v
